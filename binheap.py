da5d21101ec4a608519b388fe6ec1969c7a388ec
Nikita <basynin.nikita@gmail.com>
2019-10-30 16:41:16 +0300

    test pych
    Signed-off-by: Nikita <basynin.nikita@gmail.com>

--- binheap.py
+++ binheap.py
@@ -1,8 +1,37 @@
 class Heap:
+    def __init__(self):
+        self.elements = list()
+        self.idx = dict()
 
-    def test(self):
-        print('made')
+    def add(self, key,value):
+        if self.idx.get(key) != 0:
+            return 'error'
+        self.elements.append([key,value])
+        self.idx[key] = len(self.elements) - 1
+        #siftup
+        return ''
+    def delete(self, key):
+        return True
+
+    def min(self):
+        return self.elements[0]
+        #eto bred
+
+    def max(self):
+        return True
+
+    def set(self, key, value):
+        return True
+
+    def search(self, key):
+        return True
+
+    def extract(self):
+        return True
+
+    def print(self):
+        return True
+
 
 
 h = Heap()
-h.test()
