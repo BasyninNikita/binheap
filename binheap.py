17d3aa0dfcf85f0fcf85fe87ed95ccdc902276f4
Nikita <basynin.nikita@gmail.com>
2019-11-06 18:07:08 +0300

    need I change min and max?
    Signed-off-by: Nikita <basynin.nikita@gmail.com>

--- binheap.py
+++ binheap.py
@@ -1,29 +1,64 @@
+class Node:
+    def __init__(self, key, value, ):  # index):
+        self.key = key
+        self.value = value
+        # self.index = index
+
+
 class Heap:
     def __init__(self):
         self.elements = list()
-        self.idx = dict()
+        self.indexes = dict()
 
-    def add(self, key,value):
-        if self.idx.get(key) != 0:
-            return 'error'
-        self.elements.append([key,value])
-        self.idx[key] = len(self.elements) - 1
-        #siftup
-        return ''
-    def delete(self, key):
-        return True
+    def siftup(self, idx):
+        while self.elements[idx].key < self.elements[((idx - 1) // 2)].key:
+            if idx == 0:
+                break
+            idx_of_parent = (idx - 1) // 2
+            self.elements[idx], self.elements[idx_of_parent] = self.elements[idx_of_parent], self.elements[idx]
+            self.indexes[self.elements[idx].key], self.indexes[self.elements[idx_of_parent].key] = \
+                self.indexes[self.elements[idx_of_parent].key], self.indexes[self.elements[idx].key]
+            idx = (idx - 1) // 2
+        # return True
 
-    def min(self):
-        return self.elements[0]
-        #eto bred
-
-    def max(self):
+    def add(self, key, value):
+        if self.indexes.get(key):
+            return False
+        x = Node(key, value)  # , len(self.elements))
+        idx = len(self.elements)
+        idx_of_parent = (idx - 1) // 2
+        self.indexes[key] = len(self.elements)
+        self.elements.append(x)
+        if idx > 0 and x.key < self.elements[idx_of_parent].key:
+            self.siftup(idx)
         return True
 
     def set(self, key, value):
-        return True
+        if self.indexes.get(key):
+            self.elements[self.indexes.get(key)].value = value
+            return True
+        else:
+            return False
 
     def search(self, key):
+        return self.elements[self.idx.get(key)].key, self.elements[self.idx.get(key)].value if self.idx.get(key) else None, None
+
+    def min(self):
+        return 'error' if len(self.elements) == 0 else (str(self.elements[0].key) + '0' + self.elements[0].value)
+        # return self.elements[0]
+        # eto bred
+
+    def max(self):
+        if len(self.elements) == 0:
+            return 'error'
+        x = max(self.elements)
+        return str(x.key) + ' ' + str(self.indexes.get(x.key)) + ' ' + x.value
+        # return True
+
+    def delete(self, key):
+        return True
+
+    def heapify(self):
         return True
 
     def extract(self):
@@ -33,5 +68,6 @@ class Heap:
         return True
 
 
-
 h = Heap()
+h.add(1, '11')
+h.add(2, '22')
